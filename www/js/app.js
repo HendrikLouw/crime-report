197dd4cc16adb542bea531228f0b5934521b6f09
Hendrik Louw <hflouw@gmail.com>
2014-10-27 10:51:24 +0200

    use google maps starter

--- www/js/app.js
+++ www/js/app.js
@@ -1,24 +1,9 @@
-angular.module('crime-report', ['ionic', 'leaflet-directive'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
 })
-.controller('ViewMapCtrl', function($scope) {
-   angular.extend($scope, {
-      center: {
-        lat: 52.52,
-        lng: 13.40,
-        zoom: 14,
-        autoDiscover: true
-      }
-  });
-});
