689242a8f03b73afbd7447ec7a902a4136e42d73
simnyatsanga <simnyatsanga@gmail.com>
2014-10-29 14:54:25 +0200

    adding dummy test

--- test/map/mapCtrlSpec.js
+++ test/map/mapCtrlSpec.js
@@ -1,4 +1,8 @@
 describe('MapCtrl', function() {
 
   beforeEach(module('crime-report.map.controllers'));
+  it('should do stuff', function(){
+    console.log('doing stuff');
+    expect(true).toBeTruthy();
+  });
 });
